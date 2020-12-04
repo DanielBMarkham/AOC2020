3a25dfbe81e0f2ea31680989467e03cc8a767e2c
Daniel Markham <danielbmarkham@hotmail.com>
2020-12-04 11:29:28 -0500

    Part 1 done. Needs cleanup

--- Program.fs
+++ Program.fs
@@ -2,36 +2,45 @@ open System
 open TestData
 open Microsoft.FSharp.Collections
 
+let flipArray(arr):'A[,]= Array2D.init (arr |> Array2D.length2) (arr |> Array2D.length1) (fun r c -> arr.[c,r])
+let inline flatten (A:'a[,]) = A |> Seq.cast<'a>
+let inline getColumn c (A:_[,]) = flatten A.[*,c..c] |> Seq.toArray
+let inline getRow r (A:_[,]) = flatten A.[r..r,*] |> Seq.toArray
+///
+/// Many thanks to Tomas Petricek
+///
+let rec repeat items =
+  seq { yield! items
+        yield! repeat items }
+
 let stopWatch = System.Diagnostics.Stopwatch()
 stopWatch.Start()
 //
 // DAY 3 ??
 //
 
-let inline flatten (A:'a[,]) = A |> Seq.cast<'a>
-let inline getColumn c (A:_[,]) = flatten A.[*,c..c] |> Seq.toArray
-let inline getRow r (A:_[,]) = flatten A.[r..r,*] |> Seq.toArray
-let rec repeat items =
-  seq { yield! items
-        yield! repeat items }
+let translateLine (line:string):int[] =
+    line.ToCharArray()|>Array.map(fun x->if x='.' then 0 else 1)
+let translateGenerators (dat:string list) =
+    dat|>List.map (translateLine)
+let rowGenerators =
+    let t=part1Data |> translateGenerators
+    t|>List.map repeat
 
-let rowGeneratorArray =[|
-    repeat [1;0;0;1]
-    ;repeat [0;1;0]
-    ;repeat [1;0]
-    ;repeat [1;0;0;1;1;0;0;1]
-    |]
+
 let columns=Seq.initInfinite(fun i->
-    rowGeneratorArray|>Seq.map(fun x->
+    //rowGeneratorArray|>Seq.map(fun x->
+    rowGenerators|>Seq.map(fun x->
         x|>Seq.skip i|>Seq.head
         )|>Seq.toList
     )
+let originalColumns=columns
 let processColumn(downIndex,column) =
     let ret=column|>List.mapi(fun i x->
         if i=downIndex+1 then x+2 else x
         )
     ret
-let nextMove=(2,1)//seq{while true do yield (1,1)}
+let nextMove=(3,1)//seq{while true do yield (1,1)}
 let processAMove(cols,iCount) =
     cols
     |> Seq.skip (fst nextMove*iCount)
@@ -42,44 +51,72 @@ let processAMove(cols,iCount) =
         )
     |>Seq.toList
 
+let printHill(samp:int [,] ) =
+    //[0..(samp.[*,0]).Length-1]
+    [1..samp|>Array2D.length2]
+    |>List.iteri(fun i x->
+        samp.[*,i]
+        |> Array.iter(fun y->
+            let c=
+                match y with
+                    |0->"."
+                    |1->"#"
+                    |2->"O"
+                    |3->"X"
+            printf "%s " c)
+        printfn ""
+        )
+let foo:list<int>=
+    let thisCol=columns|>Seq.head
+    processColumn((-1),thisCol)
+let bar:list<list<int>>=[foo]
+let bell:seq<list<list<int>>>=seq{bar}
+
 [<EntryPoint>]
 let main argv =
     printfn "Advent of Code Puzzle Output"
     printfn "Day 2"
 
-    let flipArray(arr) = Array2D.init (arr |> Array2D.length2) (arr |> Array2D.length1) (fun r c -> arr.[c,r])
-
-    let n1=processAMove(columns,0)
-    let n2=processAMove(columns,1)
-    let n3=processAMove(columns,2)
-    let n4=processAMove(columns,2)
-    let n5=processAMove(columns,2)
-
-    let samp1=columns|>Seq.truncate (fst nextMove*5)
-    let samp12D=flipArray(array2D samp1)
-    let samp2=[n1;n2;n3;n4;n5] |> List.concat
-    let samp22D=flipArray(array2D samp2)
-
+    
     let mutable index=(-1)
     let totalMoves = seq{
-        while (snd nextMove*index)<(rowGeneratorArray.Length)
+        //yield processAMove(columns, 1);
+        //yield bar;
+        yield [foo]
+        let newcol=(columns|>Seq.skip 1)
+        while (snd nextMove*index)<(rowGenerators.Length)
             do
             index<-index+1
-            yield processAMove(columns,index)
+            yield processAMove(newcol,index)
     }
 
     let samp4=totalMoves|>Seq.concat
-    let samp42D=flipArray(array2D samp4)
+    //let samp4arr=array2D samp4
+    let samp42D=array2D samp4
+    //let samp42D=flipArray(samp4arr)
+    let origArrayCols=originalColumns|>Seq.take samp42D.[0,*].Length
+    let origHill=array2D origArrayCols
+    let boo(samp:int [,]) =
+        [0..(samp.[*,0]).Length-1]
+        |>List.mapi(fun i x->
+            origHill.[*,1]
+        )
+    printHill(origHill)
+    printfn ""
+    printfn ""
+    printfn ""
+    printHill(flipArray (flipArray samp42D))
+    printfn ""
+    printfn ""
+    printfn ""
+    let mop=samp42D |> Seq.cast<int>|>Seq.toArray
+    printfn "asdf %A" (mop.Length)
+    let bop=mop|>Array.filter(fun x->x=3)
+    printfn "TOTAL TREES HIT %A" (bop.Length) // (boo |> Seq.length)
+    printfn ""
+    printfn ""
 
-    printfn "%A" samp12D
     printfn ""
-    printfn ""
-    printfn ""
-    printfn "%A" samp22D
-    printfn ""
-    printfn ""
-    printfn ""
-    printfn "%A" samp42D
 
     let ts=stopWatch.Elapsed
     let elapsedTime =
