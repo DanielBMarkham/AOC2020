f529c021be5876671fe8b4dfb2f311470dfe9a15
Daniel Markham <danielbmarkham@hotmail.com>
2020-12-11 15:41:30 -0500

    Fixed problem with the definition of the word adjacent

--- Program.fs
+++ Program.fs
@@ -2,28 +2,30 @@ open System
 open TestData
 open Microsoft.FSharp.Collections
 
-
-
 let stopWatch = System.Diagnostics.Stopwatch()
 stopWatch.Start()
 //
-// DAY 3 :38.03
+// DAY 11
 //
-
 
 [<EntryPoint>]
 let main argv =
     printfn "Advent of Code Puzzle Output"
-    printfn "Day "
+    printfn "Day 11"
 
-    // let printIfSumMatches (lis:array<'a>)=
-    //   if lis|>Array.sum=magicNumber
-    //     then printfn "%A" lis
-    //     else ()
-    seats|>seatPeople|>seatPeople
-    //let foo= m|>allContiguousSegments printIfSumMatches
-    //printM numArr
-//    ans m
+    let rec seatingLoop i sts oldTotal =
+        let newI=i+1
+        if i>Int32.MaxValue then failwith "i too big" else ()
+        showSeats sts
+        printfn ""
+        let n=seatPeople(sts)
+        let m=n|>Seq.cast<SeatType>|>Seq.filter(fun x->x=Occupied) |> Seq.length
+        if oldTotal<>m
+            then seatingLoop newI n m
+            else oldTotal
+
+    let equibSeatCount=seatingLoop 0 seats -1
+    printfn "%A" equibSeatCount
 
 
     let ts=stopWatch.Elapsed
--- TestData.fs
+++ TestData.fs
@@ -26,9 +26,9 @@ let typeToLetter (s:SeatType)=
     |Occupied->"#"
     |Floor->"."
 let showSeats (str:SeatType[,])=
-  [0..str |>Array2D.length1]
-  |> List.iteri(fun i x->
-    let row:SeatType array=getRow i str
+  [0..((str |>Array2D.length1)-1)]
+  |> List.iter(fun x->
+    let row:SeatType array=str.[x,0..]
     row
     |>Array.iter(fun (y:SeatType)->
         let ltr=(typeToLetter y)
@@ -37,6 +37,25 @@ let showSeats (str:SeatType[,])=
       )
     printfn ""
     )
+let adjacentSeats x y (seatArray:SeatType[,]) =
+  let getSeat(x,y)=
+    let l1=(seatArray |> Array2D.length1)
+    let l2=(seatArray |> Array2D.length2)
+    if x<0 || y<0 || x>l1-1 || y>l2-1 then None else Some seatArray.[x,y]
+
+  let topLeft=getSeat(x-1,y-1)
+  let topCenter=getSeat(x,y-1)
+  let topRight=getSeat(x+1,y-1)
+  let lft=getSeat(x-1,y)
+  let rgt=getSeat(x+1,y)
+  let bottomLeft=getSeat(x-1,y+1)
+  let bottomCenter=getSeat(x,y+1)
+  let bottomRight=getSeat(x+1,y+1)
+  [|
+    topLeft;topCenter;topRight;lft;rgt;bottomLeft;bottomCenter;bottomRight
+  |] |> Array.choose id
+
+
 let seats=
 
   stringSeats
@@ -47,14 +66,12 @@ let seats=
       |"#"->Occupied
       |_->Floor
     )
-let seatPeople (sts:SeatType[,])=
+let seatPeople(sts:SeatType[,])=
   let ret=
     sts
     |>Array2D.mapi(fun x y thisSeat->
-      let peopleToMyLeft=sts.[..x,y]|>Seq.filter(fun z->z=Occupied)|>Seq.length
-      let peopleToMyRight=sts.[x..,y]|>Seq.filter(fun z->z=Occupied)|>Seq.length
-      let totalRowCount=peopleToMyLeft+peopleToMyRight
-      printfn "  -  %A" totalRowCount
+      let seatsAroundMe = adjacentSeats x y sts
+      let totalRowCount=seatsAroundMe|>Seq.filter(fun z->z=Occupied)|>Seq.length
       let newSeat=
         match sts.[x,y] with
           |Empty when totalRowCount=0->Occupied
